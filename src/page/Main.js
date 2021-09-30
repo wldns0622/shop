5d12a01f82cb3426db46586eb1231cd52920266f
wldns0622 <wldns12378@gmail.com>
2021-09-30 18:47:10 +0900

    feat: Main Page 생성

---
+++ src/page/Main.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import styled from 'styled-components';
+import data from '../data';
+import Shoes from '../components/Shoes';
+
+const Wrapper = styled.section`
+padding: 4em;
+background-color: #eceef2;
+text-align: center;
+`;
+
+const Title = styled.h1`
+color: #4572fe;
+font-size: 4rem;
+font-weight: 700;
+`;
+
+const Description = styled.p`
+margin: 1em 0;
+font-size: 1.2em;
+`;
+
+function Main() {
+  let [shoes, setShoes] = useState(data);
+
+
+  return (
+    <>
+      <Wrapper className="background">
+        <Title>
+          Hello World!
+        </Title>
+        <Description>
+          helloodjadlajdlkasjdklsa
+        </Description>
+        <Button variant="primary" size="lg">
+          Large button
+        </Button>
+      </Wrapper>
+      <div className="container">
+        <ul style={{listStyle: 'none'}} className="row">
+          {shoes.map((item) => (<Shoes key={item.id} data={item} />))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Main;
