0a08f054fea9dfd89af519e3492d7132e602f0f8
wldns0622 <wldns12378@gmail.com>
2021-09-30 19:05:27 +0900

    feat: 라우터 설정

--- src/page/App.js
+++ src/page/App.js
@@ -10,17 +10,22 @@ function App() {
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+          <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
+          <Nav.Link >Pricing</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
 
-      <Route exact path="/" component={Main} />
-      <Route path="/detail" component={Detail} />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/detail" component={Detail} />
+        <Route path="/:id">
+          <div>아무거나적었을때 이거 보여주셈</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,4 +1,8 @@
+import {useHistory} from 'react-router-dom';
+
 function Detail() {
+  let history = useHistory();
+
   return (
     <div className="container">
       <div className="row">
@@ -10,6 +14,7 @@ function Detail() {
           <p>상품설명</p>
           <p>120000원</p>
           <button className="btn btn-danger">주문하기</button> 
+          <button className="btn btn-danger" onClick={()=> { history.goBack() }}>뒤로가기</button> 
         </div>
       </div>
     </div> 
