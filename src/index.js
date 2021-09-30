528e0ff5dbc2ad05cffee1b18838ce741c823692
wldns0622 <wldns12378@gmail.com>
2021-09-30 18:19:09 +0900

    feat: router 모듈 설치

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './page/App';
 
+// HashRouter 대체 가능
+// HashRouter은 조금더 라우팅을 안정하게 할 수 있음
+// 주소창은 서버에게 리소스를 연결하는 곳이다.
+// Hash라우터는 실수로 서버에 요청을 하지 않기위해서 사용함
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
